d3fab16d05691b08f0486d54e4bd5cafedbc4949
vadim <vadimt2@gmail.com>
2020-07-30 17:46:04 +0300

    minor changes in homepage

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -20,22 +20,15 @@ export class HomeComponent implements OnInit {
   constructor(private store$: Store<RootStoreState.State>, private photoService:PhotoService){
   }
 
-  public photos:IPhoto[] = [];
   public photos$: Observable<IPhoto[]>;
   public error$: Observable<string>;
   public isLoading$: Observable<boolean>;
-  public editPhoto:boolean;
   public photoToUpdate:IPhoto;
 
   ngOnInit() {
     this.photos$ = this.store$.select(
       PhotoStoreSelectors.selectPhotos
     );
-
-
-    this.photos$.subscribe(data => {
-      this.photos = data;
-    });
 
     this.error$ = this.store$.select(
       PhotoStoreSelectors.selectPhotosError
